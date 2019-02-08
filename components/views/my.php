c0036aa0b36fb1878fd52f4457c755e2f98784ff
Hook-Ti <davletshin.talan@gmail.com>
2019-02-08 18:05:16 +0500

    Friday added new changes

---
+++ components/views/my.php
@@ -0,0 +1,3 @@
+<h2>Hello world!!</h2>
+<?=	$content;?>
+<h2> Here is greeting from widget component</h2>
--- views/new/forms.php
+++ views/new/forms.php
@@ -9,10 +9,23 @@ use yii\helpers\Html;
 		<div class=" col-md-6 offset-md-1">
 			
 <?php 
+
+//use app\components\MyWidget;
+/**
+* My own widget
+*/
+
+app\components\MyWidget::begin();?>
+<h1>Here is some text</h1>
+<?php app\components\MyWidget::end();
+
 $form = ActiveForm::begin(['options'=>['class'=>'form-horizontal']])?>
 <?= $form->field($model,'name')->label('Имя'); ?>
 <?= $form->field($model,'email')->label('Email')->input('email'); ?>
 <?= $form->field($model,'password')->label('Пароль')->passwordInput(); ?>
+
+<?= yii\jui\DatePicker::widget(['name'=>'attributeName']); ?> //Datepicker
+
 <?= $form->field($model,'text')->label('Текст сообщения')->textarea(['rows'=>5]); ?>
 <?= Html::activeCheckbox($model,'remember',[]);?>
 <?= Html::submitButton('Отправить',['class'=>'btn btn-success']);?>
